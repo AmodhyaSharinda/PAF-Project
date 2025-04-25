8ec92b59e6bfb27a5066dbaf6e1beef55936d4c2
AmodhyaSharinda <sharinda72@gmail.com>
2025-04-26 01:33:16 +0530

    create and Read of users

--- Backend/src/main/java/com/skillsharing/controller/user/userControler.java
+++ Backend/src/main/java/com/skillsharing/controller/user/userControler.java
@@ -1,17 +1,33 @@
 package com.skillsharing.controller.user;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.skillsharing.dto.user.userDTO;
+import com.skillsharing.services.user.userService;
 
 @RestController
 @CrossOrigin
 @RequestMapping(value = "/api/users/")
 public class userControler {
 
-    @GetMapping("/getUser")
-    public String getUsers(){
-        return "One user";
+    @Autowired
+    private userService userService;
+
+    @GetMapping("/getusers")
+    public List<userDTO> getUsers(){
+        return userService.getAllUsers();
+    }
+
+    @PostMapping("/adduser")
+    public userDTO saveUser(@RequestBody userDTO userDTO){
+        return userService.createUser(userDTO);
     }
 }
--- Backend/src/main/java/com/skillsharing/services/user/userService.java
+++ Backend/src/main/java/com/skillsharing/services/user/userService.java
@@ -1,5 +1,33 @@
 package com.skillsharing.services.user;
 
-public class userService {
+import java.util.List;
 
+import org.modelmapper.ModelMapper;
+import org.modelmapper.TypeToken;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.skillsharing.dto.user.userDTO;
+import com.skillsharing.model.user.user;
+import com.skillsharing.repository.user.userRepo;
+
+@Service
+@Transactional
+public class userService {
+    @Autowired
+    private userRepo userRepo;
+
+    @Autowired
+    private ModelMapper modelMapper;
+
+    public List<userDTO> getAllUsers(){
+        List<user> userList = userRepo.findAll();
+        return modelMapper.map(userList, new TypeToken<List<userDTO>>(){}.getType());
+    }
+
+    public userDTO createUser(userDTO userDTO){
+        userRepo.save(modelMapper.map(userDTO, user.class));
+        return userDTO;
+    }
 }
