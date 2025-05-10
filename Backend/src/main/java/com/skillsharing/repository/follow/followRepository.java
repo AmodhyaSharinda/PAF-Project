9485334f0c46f60fd92369e1544e826854422724
AmandaArangalla <arangallaamanda@gmail.com>
2025-05-10 19:31:25 +0530

    follower count

--- Backend/src/main/java/com/skillsharing/repository/follow/followRepository.java
+++ Backend/src/main/java/com/skillsharing/repository/follow/followRepository.java
@@ -6,7 +6,7 @@ import java.util.Optional;
 //import org.bson.types.ObjectId;
 //import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.repository.MongoRepository;
-//import org.springframework.data.mongodb.repository.Query;
+//port org.springframework.data.mongodb.repository.Query;
 //import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
